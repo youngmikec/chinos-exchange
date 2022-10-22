9a17bf90c7d9c6040497b6964a9c6c0f3a7abd56
Charles-04 <charlesmir04@gmail.com>
2022-10-22 16:18:52 +0100

    contact us work done

--- src/components/frontend-components/contact-us-comp.tsx
+++ src/components/frontend-components/contact-us-comp.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 // style
 import './style.css';
@@ -8,6 +8,7 @@ import coin from '../../assets/images/hompage_coin.png'
 import { BsTelephoneFill } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
 import {BsEnvelopeFill} from "react-icons/bs";
+
 // components
 import HeroSection from '../../shared/users-frontend/hero-section';
 import { Link } from 'react-router-dom';
@@ -15,6 +16,9 @@ import JoinUs from '../../shared/users-frontend/join-us';
 import Footer from '../../shared/users-frontend/footer';
 
 const ContactUsComp = () => {
+const[toggle,setToggle] = useState(true)
+const[toggle1,setToggle1] = useState(true)
+const[toggle2,setToggle2] = useState(true)
   return (
     <>
         <HeroSection>
@@ -36,21 +40,64 @@ const ContactUsComp = () => {
                 24/7 available 
                 </h3>
             </div>
-            <div className="flex flex-row justify-evenly">
-                <div className=" flex flex-col justify-center social-box w-2/12 hover:rounded-lg ">
+            <div className=' flex flex-row justify-evenly z-10 my-20' >
+                <div 
+                className={toggle? 'flex flex-col justify-center social-box w-2/12 hover:rounded-lg':'flex flex-col justify-center social-box-hover w-2/12 hover:rounded-lg'}
+                onMouseLeave={()=>setToggle(true)} onMouseOver={()=>setToggle(false)}
+                >
                    <BsWhatsapp className='mx-auto mt-4 text-3xl'/>
 
-                   <h3 className='text-center my-8 '>+234 703 162 5759</h3>
+                   <h3 className={toggle ? 'text-center my-8 social-box-text':'text-center my-8 social-box-hover-text'}>+234 703 162 5759</h3>
                 </div>
-                <div className=" flex flex-col justify-center social-box w-2/12 hover:rounded-lg">
+                <div 
+                className={toggle1? 'flex flex-col justify-center social-box w-2/12 hover:rounded-lg':'flex flex-col justify-center social-box-hover w-2/12 hover:rounded-lg'}
+                onMouseLeave={()=>setToggle1(true)} onMouseOver={()=>setToggle1(false)}
+                >
                     <BsEnvelopeFill className='mx-auto mt-4 text-3xl'/>
-                    <h3 className='text-center my-8 w-full '>chinoexchange@gmail</h3>
+                    <h3 className={toggle1 ? 'text-center my-8 social-box-text':'text-center my-8 social-box-hover-text'}>chinoexchange@gmail</h3>
                 </div>
-                <div className=" flex flex-col justify-center social-box w-2/12 hover:rounded-lg">
+                <div
+                 className={toggle2? 'flex flex-col justify-center social-box w-2/12 hover:rounded-lg':'flex flex-col justify-center social-box-hover w-2/12 hover:rounded-lg'}
+                 onMouseLeave={()=>setToggle2(true)} onMouseOver={()=>setToggle2(false)}
+                 >
                     <BsTelephoneFill className='mx-auto mt-4 text-3xl'/>
-                    <h3 className='text-center my-8 '>+234 703 162 5759</h3>
+                    <h3 className={toggle2 ? 'text-center my-8 social-box-text':'text-center my-8 social-box-hover-text'}>+234 703 162 5759</h3>
                 </div>
 
+            </div>
+            <div className='flex flex-col justify-center px-0 shadow-lg w-11/12 mx-auto mt-20'>
+
+                <div className="flex w-9/12 justify-between mx-auto my-4">
+                   
+                    <div className='flex flex-col w-5/12 '>
+                    <label htmlFor="" className=''>Full name</label> 
+                    <input type="text" name="" id="" className='input-border rounded-md py-3 outline-none'/>
+                    </div>
+                   
+                    <div className='flex flex-col w-5/12'>
+                    <label htmlFor="">Phone Number </label> 
+                    <input type="text" name="" id="" className='input-border rounded-md py-3 outline-none'/>
+                    </div> 
+                </div>
+                <div className="flex w-9/12 justify-between mx-auto my-4">
+                    
+                    <div className='flex flex-col w-5/12'>
+                    <label htmlFor="">Email Address</label> 
+                    <input type="text" name="" id="" className='input-border rounded-md py-3  outline-none'/>
+                    </div>
+                    
+                    <div className='flex flex-col w-5/12 '>
+                    <label htmlFor="">Subject</label> 
+                    <input type="text" name="" id="" className='input-border rounded-md py-3 outline-none'/> 
+                    </div>
+                </div>
+                <div className="flex w-9/12 mx-auto my-4">
+                
+                <textarea name="" id="" className='input-border scrollable-none py-5 w-full rounded-md outline-none mt-4'></textarea>
+                </div>
+                <div className='flex w-9/12 justify-end my-10 self-center '>
+                    <input type="button" value=" Send Message"  className='button rounded-md ' />
+                </div>
             </div>
         </div>
         <div className='my-4'>
