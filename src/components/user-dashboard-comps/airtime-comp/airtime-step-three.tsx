1a4a39b086240963076bca421826432784ab399b
youngmikec <michaelozor15@gmail.com>
2022-10-31 14:01:31 +0100

    added airtime and sell crypto steps

---
+++ src/components/user-dashboard-comps/airtime-comp/airtime-step-three.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const AirtimeStepThree = () => {
+    return (
+        <>
+            <div className='w-full'>
+                <div className='my-4'>
+                    <label htmlFor="accountNumber" className='text-[#7F7F80] text-sm'>Kindly upload a proof your transaction</label>
+                    <div className='border-2 border-gray-100 rounded-md mt-2 h-32 w-full flex justify-center'>
+                        <button className='text-center text-[#7F7F80]'>
+                            + <br /> Choose file
+                        </button>
+                        <input type="file" className='hidden' />
+                    </div>
+                </div>
+
+                
+                <div className='my-4 flex justify-center'>
+                    <button className='rounded-md bg-[#8652A4] text-white px-6 py-3'>Mark Paid</button>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AirtimeStepThree;
--- src/components/user-dashboard-comps/sell-crypto-comp/index.tsx
+++ src/components/user-dashboard-comps/sell-crypto-comp/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Step } from '../../../common';
+import Card from '../../../shared/card';
 import StepHeader from '../../../shared/step-header';
+import SellCryptoStepOne from './sell-crypto-step-one';
 
 const SellCryptoComp = () => {
     const steps: Step[] = [
@@ -31,8 +33,54 @@ const SellCryptoComp = () => {
                         steps={steps}
                         info="While trading with us, make sure you copy the right address for your deposit to avoid lost of fund. upload a screenshot of your transaction as well."  
                     />
+
                 </div>
-                <div></div>
+            </div>
+
+            <div className='flex flex-row my-4'>
+                <div className='w-8/12 ml-auto mr-8'>
+                    {/* <h3 className="mb-3 text-[#7F7F80]">Select Provider</h3> */}
+                    <Card type="lg">
+                        {/* airtime steps */}
+                        <SellCryptoStepOne />
+
+                        {/* airtime steps */}
+                    </Card>
+                </div>
+
+                <div className='w-4/12'>
+                    <Card type="lg">
+                        {/* Crypto rates */}
+                        
+                        <div>
+                            <div className="flex justify-between my-3">
+                                <div><p className='text-[#7F7F80]'><strong>Name</strong></p></div>
+                                <div><p className='text-[#7F7F80]'><strong>Rate</strong></p></div>
+                            </div>
+                            <div className="flex justify-between my-3">
+                                <div><p className='text-[#7F7F80]'><strong>USDT</strong></p></div>
+                                <div><p className='text-[#8652A4] font-thin'><strong>730/$</strong></p></div>
+                            </div>
+                            <div className="flex justify-between my-3">
+                                <div><p className='text-[#7F7F80]'><strong>BNB</strong></p></div>
+                                <div><p className='text-[#8652A4] font-thin'><strong>730/$</strong></p></div>
+                            </div>
+                            <div className="flex justify-between my-3">
+                                <div><p className='text-[#7F7F80]'><strong>ETH</strong></p></div>
+                                <div><p className='text-[#8652A4] font-thin'><strong>730/$</strong></p></div>
+                            </div>
+                            <div className="flex justify-between my-3">
+                                <div><p className='text-[#7F7F80]'><strong>BTC</strong></p></div>
+                                <div><p className='text-[#8652A4] font-thin'><strong>730/$</strong></p></div>
+                            </div>
+                            <div className="flex justify-between my-3">
+                                <div><p className='text-[#7F7F80]'><strong>BTC</strong></p></div>
+                                <div><p className='text-[#8652A4] font-thin'><strong>730/$</strong></p></div>
+                            </div>
+                        </div>
+                        {/* Crypto rates */}
+                    </Card>
+                </div>
             </div>
         </>
     )
